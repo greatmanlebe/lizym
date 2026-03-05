3082189102d4edfe1b2cee796480ee318d6781cf
greatmanlebe <lebegreatman00@gmail.com>
2026-03-05 17:29:41 +0300

    modified:   resources/js/i18n.js

--- resources/js/i18n.js
+++ resources/js/i18n.js
@@ -1,12 +1,10 @@
-import { createI18n } from 'vue-i18n'
+import { createI18n } from 'vue-i18n/dist/vue-i18n.esm-bundler.js'
 import en from './lang/en.json'
 import fr from './lang/fr.json'
 
-export default function setupI18n(locale = 'en') {
-  return createI18n({
-    legacy: false,
-    locale,
-    fallbackLocale: 'en',
-    messages: { en, fr }
-  })
-}
+export default createI18n({
+  legacy: false,
+  locale: 'en',
+  fallbackLocale: 'en',
+  messages: { en, fr }
+})
