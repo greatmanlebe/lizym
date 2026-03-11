e913bdff7221933838d6b864dd882d368a007c2c
greatzcodes <158315111+greatmanlebe@users.noreply.github.com>
2026-03-11 18:16:12 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -24,4 +24,17 @@ export default defineConfig({
             formVariants: true,
         }),
     ],
+     server: {
+        https: true,
+        host: '0.0.0.0'
+    },
+    build: {
+        // Force HTTPS in production
+        manifest: true,
+        outDir: 'public/build',
+        assetsDir: 'assets'
+    },
+    define: {
+        'import.meta.env.VITE_APP_URL': JSON.stringify('https://lizym.onrender.com')
+    }
 });
